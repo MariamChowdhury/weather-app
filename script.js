6c820cb63550710bb2a5d770b00c7e97dc2b3e64
Mariam <mariamchowdhury.200@gmail.com>
2021-05-11 16:30:11 +0600

    done

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ const search=(city) => {
   const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
   fetch(url)
   .then(res => res.json())
-  .then (data => console.log(data))
+  .then (data => {
+    console.log(data);
+    const temperature=data.main.feels_like-273.15;
+    const newTemp=temperature.toFixed(2)
+    document.getElementById('city-name').innerText=data.name||"Unknown Location!";
+    document.getElementById('city-temp').innerText=newTemp+' °C'
+    document.getElementById('city-desc').innerText=data.weather[0].description
+    document.getElementById('city-img').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+  })
 }
 
 
